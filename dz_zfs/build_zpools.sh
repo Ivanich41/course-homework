7817f5f3663e1ce70f5d2024a6db974c9671bcfb
Ivanich41 <cooldude412003@gmail.com>
2024-06-07 15:07:35 +0300

    Added folowing homeworks: zfs nfs grub

---
+++ dz_zfs/build_zpools.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+zpool create otus1 mirror /dev/sdb /dev/sdc
+zpool create otus2 mirror /dev/sdd /dev/sde
+zpool create otus3 mirror /dev/sdf /dev/sdg
+zpool create otus4 mirror /dev/sdh /dev/sdi
